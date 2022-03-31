313aa765788626cedbbd5aec05521118d8e2ecb9
royjong <100949419+royjong@users.noreply.github.com>
2022-03-31 17:12:12 +0200

    port

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 const express = require('express'); 
 const morgan = require('morgan'); 
 
+
+const app = express(); 
+
+
+const port = 3000; 
+app.listen(port, () => console.log('Running on port' + port))
